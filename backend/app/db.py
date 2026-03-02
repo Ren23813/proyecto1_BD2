39925e504e16eff6cb35852a623a1273d5802179
Renato Rojas <roj23813@uvg.edu.gt>
2026-03-01 19:31:14 -0600

    infraestructura de docker

---
+++ backend/app/db.py
@@ -0,0 +1,7 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+
+MONGO_URL = os.getenv("DATABASE_URL")
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.get_database("nombre_de_tu_db")
