ed2dd9718da76f8640f8017f24aed6c72100e2fb
Renato Rojas <roj23813@uvg.edu.gt>
2026-03-08 13:08:28 -0600

    solucion error de lib gridfs

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,7 +2,7 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
 
 MONGO_URL = os.getenv("DATABASE_URL")
-fs = AsyncIOMotorGridFSBucket(db)
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.get_database("Proyecto1")
+fs = AsyncIOMotorGridFSBucket(db)
